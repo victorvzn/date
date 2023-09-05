c9c9ea5e325ad9741166f339b46a048a168420d9
Victor Villazon <victorzn23@gmail.com>
2023-09-04 19:51:28 -0500

    trabajando en un único handleChange

--- src/components/EmployeeForm.jsx
+++ src/components/EmployeeForm.jsx
@@ -11,32 +11,43 @@ const EmployeeForm = () => {
       salary: 0
     })
 
-    const handleChangeEmployeeName = (event) => {
+    const handleChange = (event) => {
       const input = event.target
+      const name = input.name
       const value = input.value
-      setEmployeeName(value)
+
+      setForm({
+        ...form,
+        [name]: value
+      })
     }
 
-    const handleChangeDiscount = (event) => {
-      const input = event.target
-      const value = input.value
-      setDiscount(value)
-    }
+    // const handleChangeEmployeeName = (event) => {
+    //   const input = event.target
+    //   const value = input.value
+    //   setEmployeeName(value)
+    // }
 
-    const handleChangeSalary = (event) => {
-      const input = event.target
-      const value = input.value
-      setSalary(value)
-    }
+    // const handleChangeDiscount = (event) => {
+    //   const input = event.target
+    //   const value = input.value
+    //   setDiscount(value)
+    // }
+
+    // const handleChangeSalary = (event) => {
+    //   const input = event.target
+    //   const value = input.value
+    //   setSalary(value)
+    // }
 
     const handleSubmit =(event)=>{
      event.preventDefault();
-     if(!employeeName || !discount || !salary){
+     if(!form.employeeName || !form.discount || !form.salary){
         alert("verifica los datos ingresados");
         return;
-     }else if (discount > 100 || discount <0){
+     }else if (form.discount > 100 || form.discount <0){
         const discountValidate=
-        discount < 0 
+        form.discount < 0 
         ? alert("el descuento no puede ser menor a 0")
         : alert("el descuento no debe ser mayor a 100");
         return discountValidate
@@ -50,17 +61,17 @@ const EmployeeForm = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="">
           Nombre del empleado
-          <input type="text" value={employeeName} onChange={handleChangeEmployeeName}/>
+          <input type="text" name="employeeName" value={form.employeeName} onChange={handleChange}/>
         </label>
         <br />
         <label htmlFor="">
          Descuento
-          <input type="text" value={discount} onChange={handleChangeDiscount} />
+          <input type="text" name="discount" value={form.discount} onChange={handleChange} />
         </label>
          <br />
         <label htmlFor="">
          salario
-          <input type="text" value={salary} onChange={handleChangeSalary} />
+          <input type="text" name="salary" value={form.salary} onChange={handleChange} />
         </label>
 
         <br />
